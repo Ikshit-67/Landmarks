e27854dffd4dfc6e31e47e10ecd6a9ca2deebcd1
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2023-01-12 10:29:36 +0530

    initial commit

---
+++ Landmarks/Views/TestView.swift
@@ -0,0 +1,27 @@
+//
+//  TestView.swift
+//  Landmarks
+//
+//  Created by Ikshit Chaudhari on 12/01/23.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        VStack{
+         
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text("lolm")
+            Text("lol")
+            
+        }
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
