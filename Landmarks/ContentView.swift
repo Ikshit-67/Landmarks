e546302c42bb5d5dd2be0884013d4f4a627ae07b
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2023-01-12 10:27:33 +0530

    first commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ikshit Chaudhari on 12/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+            Text("Turtle Rock")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ikshit Chaudhari on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
